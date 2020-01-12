86cc33aa93e2d242276fe542cefbff22d2f68ad3
Jonathan Hung <38867001+jonachung@users.noreply.github.com>
2020-01-11 21:37:57 -0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+keys = {
+    "product_url": "", # url link of product u want to buy
+    "name": "Jonathan",
+    "email": "email@gmail.com",
+    "phone_number": "1231231234",
+    "street_address": "1234 Cookie Lane",
+    "zip_code": "00000",
+    "card_cvv": "999",
+    "card_number": "1228199669918221"
+}
