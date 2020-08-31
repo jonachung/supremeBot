eca2a3ec0a51c790b41d8bc7a6ca93695c3c2206
Jonathan Hung <38867001+jonachung@users.noreply.github.com>
2020-08-31 11:05:29 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,14 @@
 keys = {
-    "product_url": "", # url link of product u want to buy
-    "name": "Jonathan",
+    "product_url": "", 
+    "name": "hi",
     "email": "email@gmail.com",
-    "phone_number": "1231231234",
+    "phone_number": "1234",
     "street_address": "1234 Cookie Lane",
     "zip_code": "00000",
-    "card_cvv": "999",
-    "card_number": "1228199669918221"
+    "city": "San",
+    "state": "CA",
+    "card_cvv": "000",
+    "card_number": "1228199669918221",
+    "card_month": "01",
+    "card_year": "2222"
 }
