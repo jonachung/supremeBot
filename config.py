3ca30eff5caa534888ed6fa9b4ab70152178c634
Jonathan Hung <38867001+jonachung@users.noreply.github.com>
2020-08-31 11:11:23 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 keys = {
-    "product_url": "", 
-    "name": "hi",
+    "product_url": "", # Don't touch this area.
+    "name": "i love supreme",
     "email": "email@gmail.com",
-    "phone_number": "1234",
+    "phone_number": "1234567890",
     "street_address": "1234 Cookie Lane",
     "zip_code": "00000",
-    "city": "San",
-    "state": "CA",
+    "city": "U thought",
+    "state": "NY", # Add Abbreviation of state (ex. "AL", "NY", etc.)
     "card_cvv": "000",
     "card_number": "1228199669918221",
-    "card_month": "01",
+    "card_month": "01", # Month has to be two digit format (ex. "04", "12", etc.)
     "card_year": "2222"
 }
